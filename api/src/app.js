c18e124a94b03dd3660a264b22c1dceebd7ea97a
Daniel Ward <danward@gmail.com>
2018-06-04 15:52:23 -0400

    Clean up feathers app entrypoint

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,6 @@ const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 
-
 const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
@@ -14,27 +13,16 @@ const channels = require('./channels');
 
 const app = express(feathers());
 
-// Load app configuration
 app.configure(configuration());
-// Enable security, CORS, compression, favicon and body parsing
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// Host the public folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Set up Plugins and providers
 app.configure(express.rest());
 app.configure(socketio());
 
-// Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
-// Set up our services (see `services/index.js`)
 app.configure(services);
-// Set up event channels (see channels.js)
 app.configure(channels);
-
-// Configure a middleware for 404s and the error handler
-// app.use(express.notFound());
-// app.use(express.errorHandler({ logger }));
 
 app.hooks(appHooks);
 
